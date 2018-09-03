8bb163470b7b30ae6d6c3e1f5e8327679ec481e4
LAPTOP-HEH3NR2A\victor <vcortez@spac.io>
2018-09-03 14:19:51 -0700

    added hide show functionality based on choses parameters

--- functions.js
+++ functions.js
@@ -1,3 +1,41 @@
 function alertTest () {
   swal("Awesome!", "Confirmation will be sent to your email", "success");
 };
+
+// showBuilding functions
+
+function showSurreyBuildings() {
+  $("#richmondBuildings").hide();
+  $("#langleyBuildings").hide();
+
+  $("#questionBuilding").show();
+  $("#surreyBuildings").show();
+};
+
+function showRichmondBuildings() {
+  $("#surreyBuildings").hide();
+  $("#langleyBuildings").hide();
+
+  $("#questionBuilding").show();
+  $("#richmondBuildings").show();
+};
+
+function showLangleyBuildings() {
+  $("#richmondBuildings").hide();
+  $("#surreyBuildings").hide();
+
+  $("#questionBuilding").show();
+  $("#langleyBuildings").show();
+};
+
+// showFillerFloor function
+
+function showFillerFloor () {
+  $("#questionFloor").show();
+  $("#fillerFloor").show();
+};
+
+// showButton function
+function showApplyButton () {
+  $("#applyButton").show();
+};
